ba6d6c5ad1a486e7e79600cb59f0f9837b51270a
Mihail Chirita <mihail.chirita@dot.ohio.gov>
2020-01-08 09:13:01 -0500

    Added GPU support

---
+++ build_docker_image_gpu.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+
+docker build -t mihailrc/deep-learning-gpu -f Dockerfile-gpu .
---
+++ start_docker_gpu.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+docker run --rm -it -p 8888:8888 \
+  -v $(pwd)/notebooks:/tf/notebooks -v $(pwd)/data:/data \
+  -v $(pwd)/models:/models mihailrc/deep-learning-gpu
